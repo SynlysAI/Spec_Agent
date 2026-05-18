c40d45f0d84795fe6bf22b1f6681b7cd3e00692f
lyt0615 <lyt20000615@163.com>
2026-05-18 14:49:49 +0800

    export log

--- scripts/raman_report.py
+++ scripts/raman_report.py
@@ -4,6 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
+
+def get_log(content):
+    
+    keywords = ['对焦', 'Actual exposure time', 'grating']
+    
+    log = []
+    body_id = [content.index(c) for c in content if 'body: "{' in c]
+    for i in range(0, len(body_id)-1, 2):
+        print(i)
+        log.extend(content[body_id[i]: body_id[i+1]])
+    return log
+        
+    # for c in content:
+    #     body_id = content.index('body: "{')
+    #     if '对焦' in c: log.append(c)
+    #     exp_time_id = content.index('Actual exposure time')
+    #     c.extend(content[exp_time_id:exp_time_id+4])
+        
 def get_metadata(raw_data):
     # 去掉前缀和换行符，提取 JSON 部分
     json_str = raw_data.replace('capture_settings:', '').strip()
@@ -61,4 +79,12 @@ report = {
 
 for label, value in report.items():
     print(f"  {label}: {value}")
-    
+
+# 读取日志
+with open('/home/lyt/projects/Spec_Agent/ExRaman_2026-04-23.log', 'r') as f:
+    content = f.readlines()
+log = get_log(content) # 提取与测试有关内容
+# 写入新日志文件
+with open('/home/lyt/projects/Spec_Agent/log.log', 'w') as l:
+    for c in log:
+        l.writelines(c)
